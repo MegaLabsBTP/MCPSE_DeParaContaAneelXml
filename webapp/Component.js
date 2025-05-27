bb362c89a30f8b79b4d5dc6f4b9a0c92d0f3f76e
Andre Passos de Tassis <andre.tassis@megawork.com>
2025-05-27 14:31:38 +0000

    criacao do app

---
+++ webapp/Component.js
@@ -0,0 +1,15 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("megawork.deparaaneelxml.Component", {
+            metadata: {
+                manifest: "json",
+                config:  {
+                    "fullWidth": true
+                }
+            }
+        });
+    }
+);
